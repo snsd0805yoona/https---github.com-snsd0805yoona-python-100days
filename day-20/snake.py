207b341eb576b2ddff188364d31a6f19ad3ea9f2
snsd0805yoona <snsd0805yoona0530@gmail.com>
2021-06-22 01:23:24 +0800

    making snake body to move and turn

---
+++ day-20/snake.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP=90
+DOWN=270
+RIGHT=0
+LEFT=180
+
+class Snake:
+
+    def __init__(self) -> None:
+        self.snake=[]
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for i in STARTING_POSITION:
+            segment_1 = Turtle(shape="square")
+            segment_1.speed("fastest")
+            segment_1.color("white")
+            segment_1.pu()
+            segment_1.goto(i)
+            self.snake.append(segment_1)
+
+    def move(self):
+        for seg_num in range(len(self.snake)-1 , 0 ,-1 ):
+            new_x = self.snake[seg_num-1].xcor()
+            new_y = self.snake[seg_num-1].ycor()
+            self.snake[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading()!=DOWN:
+            self.head.seth(UP)
+    def down(self):
+        if self.head.heading()!=UP:
+            self.head.seth(DOWN)
+    def right(self):
+        if self.head.heading()!=LEFT:
+            self.head.seth(RIGHT)
+    def left(self):
+        if self.head.heading()!=RIGHT:    
+            self.head.seth(LEFT)
